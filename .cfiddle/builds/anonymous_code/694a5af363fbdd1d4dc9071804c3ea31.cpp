cc2277f0c5972615d5420a9e95ffcdfe4d7ace98
Hung-Wei Tseng <activebunny@gmail.com>
2025-02-21 22:59:11 -0800

    Delete .cfiddle/builds/anonymous_code directory

--- .cfiddle/builds/anonymous_code/694a5af363fbdd1d4dc9071804c3ea31.cpp
+++
@@ -1,9 +0,0 @@
-
-extern "C" int foo(register unsigned int a, register unsigned int b){
-    return a *8;
-}
-int main(){
-    return foo(1, 0);
-}
-
-// Cfiddle-signature=694a5af363fbdd1d4dc9071804c3ea31
